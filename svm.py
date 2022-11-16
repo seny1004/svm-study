4e289d2b1c367e77e3659b66218226c3dd0534f1
seny1004 <98933920+seny1004@users.noreply.github.com>
2022-11-16 19:03:32 +0900

    Update svm.py

--- svm.py
+++ svm.py
@@ -3,65 +3,37 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+class SVM:
 
-import os
+    def __init__(self, learning_rate = 0.001,lambda_param=0.01, n_iters = 1000):
+        self.lr = learning_rate
+        self.lambda_param = lambda_param
+        self.n_iters = n_iters
+        self.w = None
+        self.b = None
 
-data = pd.read_csv("seattle-weather.csv")
+    def fit(self,X, y):
+        y_ = np.where(y <= 0, -1, 1)
+        n_samples, n_features = X.shape
 
-col_names = data.columns
+        self.w = np.zeros(n_features)
+        self.b = 0
 
-from sklearn.model_selection import train_test_split
+        #gradien decent
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                print(idx, x_i)
 
-X = data.drop(["weather","date"], axis= 1)
-y = data["weather"]
-X_train,X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state= 0)
+                #true or false
+                condition = y_[idx] * (np.dot(x_i, self.w)-self.b) >= 1
 
-cols = X_train.columns
+                if condition:
+                    self.w -= self.lr*(2 * self.lambda_param * self.w)
 
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
+                else:
+                    self.w -= self.lr * (2 * self.lambda_param * self.w - np.dot(x_i,y_[idx]))
+                    self.b -= self.lr * y_[idx]
 
-X_train = scaler.fit_transform(X_train)
-
-X_test = scaler.transform(X_test)
-
-X_train = pd.DataFrame(X_train, columns=[cols])
-X_test = pd.DataFrame(X_test, columns=[cols])
-
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-
-# class SVM:
-#
-#     def __init__(self, learning_rate = 0.001,lambda_param=0.01, n_iters = 1000):
-#         self.lr = learning_rate
-#         self.lambda_param = lambda_param
-#         self.n_iters = n_iters
-#         self.w = None
-#         self.b = None
-#
-#     def fit(self,X, y):
-#         n_samples, n_features = X.shape
-#
-#         y_ = np.where(y <= 0, -1, 1)
-#
-#         self.w = np.zeros(n_features)
-#         self.b = 0
-#
-#         for _ in range(self.n_iters):
-#             for idx, x_i in enumerate(X):
-#                 condition = y_[idx] * (np.dot(x_i, self.w)-self.b) >= 1
-#                 if condition:
-#                     self.w -= self.lr*(2*self.lambda_param * self.w)
-#                 else:
-#                     self.w -= self.lr*(2*self.lambda_param * self.w - np.dot(x_i,y_[idx]))
-#                     self.b -= self.lr*y_[idx]
-#     def predict(self, X):
-#         approx = np.dot(X, self.w) - self.b
-#         return np.sign(approx)
-svm = SVC(kernel="linear")
-svm.fit(X_train, y_train)
-
-y_pred = svm.predict(X_test)
-
-print(accuracy_score(y_test, y_pred))
+    def predict(self, X):
+        linear_output = np.dot(X, self.w) - self.b
+        return np.sign(linear_output)
